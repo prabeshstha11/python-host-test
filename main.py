e4ae6f7ec7bf6182ed688151de6d3ea4f86200d7
prabeshstha11 <prabeshshrestha90@gmail.com>
2024-11-01 10:23:48 +0545

    fixed

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import telebot
 import time
 from datetime import datetime
 import os
+import schedule
+
+def send_reminder():
+    current_hour = datetime.now().hour
+
+    if 5 <= current_hour <= 22:
+        try:
+            bot.send_message(chat_id, "Time to drink some water! Stay hydrated 💧")
+            print("Reminder sent!")
+        except Exception as e:
+            print(f"Failed to send message: {e}")
+
+def job():
+    send_reminder()
 
 bot_token = os.getenv('BOT_TOKEN')
 chat_id = os.getenv('CHAT_ID')
 
 bot = telebot.TeleBot(bot_token)
 
-def send_reminder():
-    current_hour = datetime.now().hour
+schedule.every().hour.at(":00").do(job)
 
-    if 5 <= current_hour <= 22:
-        bot.send_message(chat_id, "Time to drink some water! Stay hydrated 💧")
-
-while True:
-    print("Testing...")
-    send_reminder()
-    time.sleep(3600)
+if __name__ == "__main__":
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
